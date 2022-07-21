27eabe74e442b23994859b7044031d395fac042f
jose geraldo fernandes <macaverde604@gmail.com>
2022-07-21 20:04:24 -0300

    adiciona metodo glm

---
+++ glm.r
@@ -0,0 +1,64 @@
+rm(list=ls())
+options(warn = -1)
+
+# params
+dir_matlab_file <- 'data'
+datasets <- c('australian', 
+              'banknote', 
+              'breastcancer', 
+              'breastHess', 
+              'bupa', 
+              'climate', 
+              'diabetes', 
+              'fertility', 
+              'german', 
+              'golub', 
+              'haberman', 
+              'heart', 
+              'ILPD', 
+              'parkinsons', 
+              'sonar')
+K <- 10
+fold_n <- 1
+i <- 2
+
+# read
+print(datasets[i])
+filename <- sprintf('%s/exportBase_%s_folds_10_exec_%s.mat',
+                    dir_matlab_file, datasets[i], fold_n)
+data_mat <- R.matlab::readMat(filename)
+
+# train / test
+train <- data_mat$data[[1]]
+class_train <- data_mat$data[[2]]
+class_train[class_train == -1] = 0
+test <- data_mat$data[[3]]
+class_test <- data_mat$data[[4]]
+class_test[class_test == -1] = 0
+
+# glm
+data <- data.frame(y = class_train, x = train)
+model <- glm(class_train ~ train, data = data, family = 'binomial')
+data$y_hat <- predict(model, data, type = "response")
+
+beta_true <- model$coefficients
+
+# model
+X <- cbind(1, train)
+Y <- class_train
+
+beta_0 <- matrix(0, nrow = ncol(X), ncol = 1)
+
+epochs <- 15
+beta <- beta_0
+for (epoch in 1:epochs){
+  p <- as.vector(1 / (1 + exp(- X %*% beta)))
+  W <- p * (1 - p) * diag(length(p))
+  W_inv <- 1 / (p * (1 - p)) * diag(length(p))
+  Z <- X %*% beta + W_inv %*% (Y - p)
+  
+  beta <- solve(t(X) %*% W %*% X) %*% t(X) %*% W %*% Z
+}
+
+epsilon <- beta_true - beta
+print(epsilon)
